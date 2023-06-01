6393860a24a4096ff9961bbc83f50afb092994c6
ElderBobr <entoakk20@gmail.com>
2023-06-01 21:19:06 +0700

    Добавьте файлы проекта.

---
+++ this.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Mathem {
+    int m_value;
+public:
+    Mathem() {
+        m_value = 0;
+    }
+    Mathem& add(int value) {
+        m_value += value;
+        return *this;
+    }
+    Mathem& sub(int value) {
+        m_value -= value;
+        return *this;
+    }
+    Mathem& multiply(int value) {
+        m_value *= value;
+        return *this;
+    }
+    int getValue() {
+        return m_value;
+    }
+};
+
+int main() {
+    Mathem operation;
+    operation.add(7).sub(5).multiply(3);
+    cout << operation.getValue() << endl;
+}
